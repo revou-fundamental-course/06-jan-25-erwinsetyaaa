75d4a0703ddf8d045990df55ebc03b55706ab745
erwinsetyaaa <erwinsetya34@gmail.com>
2025-01-16 11:27:37 +0700

    Done Task

--- js/script.js
+++ js/script.js
@@ -1,22 +1,62 @@
+let indexBanner = 0;
 
-var slideIndex = 1;
-showDivs(slideIndex);
+changeBackground();
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
+function nextBanner() {
+  indexBanner = indexBanner + 1;
+  changeBackground();
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("img-slideshow");
-  if (n > x.length) {
-    slideIndex = 1;
+// function to change background banner
+function changeBackground() {
+  let bannerList = document.getElementsByClassName("img-slideshow");
+  console.log(bannerList.length);
+
+  console.log(indexBanner);
+  if (indexBanner >= bannerList.length - 1) {
+    // reset indexBanner
+    indexBanner = 0;
   }
-  if (n < 1) {
-    slideIndex = x.length;
+
+  // looping to change background
+  for (let i = 0; i < bannerList.length; i++) {
+    if (i == indexBanner) {
+      bannerList[i].style.display = "block";
+    } else {
+      bannerList[i].style.display = "none";
+    }
   }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+}
+setInterval(nextBanner, 3000);
+// Function untuk form validation
+function formValidation() {
+  let nameInput = document.getElementById("name-input").value;
+  let dateInput = document.getElementById("date-input").value;
+  let genderInput = document.getElementById("gender-input").value;
+  let messageInput = document.getElementById("message-input").value;
+
+  if (nameInput == "") {
+    alert("Nama tidak boleh kosong");
+    return false;
+  } else {
+    document.getElementById("output-name").innerHTML = nameInput;
   }
-  x[slideIndex - 1].style.display = "block";
+  if (dateInput == "") {
+    alert("Tanggal Lahir tidak boleh kosong");
+    return false;
+  } else {
+    document.getElementById("output-date").innerHTML = dateInput;
+  }
+  if (genderInput == "") {
+    alert("Jenis Kelamin tidak boleh kosong");
+    return false;
+  } else {
+    document.getElementById("output-gender").innerHTML = genderInput;
+  }
+  if (messageInput == "") {
+    alert("Pesan tidak boleh kosong");
+    return false;
+  } else {
+    document.getElementById("output-message").innerHTML = messageInput;
+  }
 }
